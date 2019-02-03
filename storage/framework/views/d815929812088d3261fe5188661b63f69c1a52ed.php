1d2e41251f8130f7c467b4c7764f97fc2f9e4e67
dimasarf <dimas.arifin97@yahoo.com>
2019-02-03 23:12:24 +0700

    Navbar sama Carousel

---
+++ storage/framework/views/d815929812088d3261fe5188661b63f69c1a52ed.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Fakultas Teknik - Universitas Riau Kepulauan</title>
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Goudy+Bookletter+1911" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link href="/css/style.css" rel="stylesheet">    
+</head>
+<body>
+    <nav class="nav justify-content-center bg-unrika-ungu nav-top">
+      <a class="nav-link " href="#">Active link</a>
+      <a class="nav-link" href="#">Link</a>
+      <a class="nav-link " href="#">Disabled link</a>
+    </nav>
+    <nav class="navbar navbar-expand-lg navbar-light bg-unrika-kuning sticky-top" id="nav-bottom">
+        <a class="navbar-brand" href="#">
+            <img src="/images/UNRIKALOGO.png" alt="" width="90em">          
+           
+            <span class="ml-3">FAKULTAS TEKNIK</span>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <div class="collapse navbar-collapse p-4 ml-auto" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item ">
+              <a class="nav-link" href="#">Profil <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarProdi" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Prodi
+                  </a>
+                <div class="dropdown-menu bg-unrika-kuning" aria-labelledby="navbarProdi" style="border: 0px">
+                    <a class="dropdown-item" href="#">Teknik Mesin</a>
+                    <a class="dropdown-item" href="#">Teknik Industri</a>
+                    <a class="dropdown-item" href="#">Teknik Arsitektur</a>
+                    <a class="dropdown-item" href="#">Teknik Sipil</a>
+                    <a class="dropdown-item" href="#">Teknik Elektro</a>
+                    
+                    
+                </div>
+            </li>
+            
+            <li class="nav-item">
+              <a class="nav-link" href="#">Kemahasiswaan</a>
+            </li>
+          </ul>          
+        </div>
+    </nav>
+
+    
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="d-block w-100" src="/images/gambar 1.jpg" alt="First slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="/images/gambar 2.jpg" alt="Second slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="/images/gambar 3.jpg" alt="Third slide">
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    
+
+</body>
+</html>
